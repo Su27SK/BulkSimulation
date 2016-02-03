6cfc3be6d380f0e57acdee9cf64cba71215fa6aa
paul <mig_29sk@163.com>
2016-02-03 13:31:47 +0800

    event driven

---
+++ Application.h
@@ -0,0 +1,19 @@
+#ifndef _APPLICATION_H_
+#define _APPLICATION_H_
+#include "Process.h"
+template<class T>
+class Application : public Process<T>
+{
+	public:
+		Application():enableRecv_(0), enableResume_(0){};
+		virtual void send(int nbytes) = 0;
+		virtual void recv(int nbytes) = 0;
+	protected:
+		virtual void start(){};
+		virtual void stop(){};
+
+		int enableRecv_;
+		int enableResume_;
+};
+
+#endif
---
+++ Process.cpp
@@ -0,0 +1,23 @@
+#include "Process.h"
+/**
+ * @brief processData 
+ * 处理数据
+ * @param {double} size
+ */
+void Process::processData(double size)
+{
+	abort();
+}
+
+/**
+ * @brief getData 
+ * 获得数据
+ * @param {double} size
+ *
+ * @return T
+ */
+T* Process::getData(double size)
+{
+	abort();
+	return NULL;
+}
