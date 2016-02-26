c057191b371966aa3a37a26603e2b121fa1ec7c0
paul <mig_29sk@163.com>
2016-02-26 13:17:27 +0800

    new file

---
+++ BulkBackHandle.cpp
@@ -0,0 +1,63 @@
+#include "BulkBackHandle.h"
+
+/**
+ * @brief start 
+ */
+void BulkBackHandle::start()
+{
+	running_ = 1;
+	double t = next();
+	timer_.sched(t);
+}
+
+/**
+ * @brief stop 
+ */
+void BulkBackHandle::stop()
+{
+	running_ = 0;
+}
+
+/**
+ * @brief timeout 
+ */
+void BulkBackHandle::timeout()
+{
+	if (running_) {
+		double t = next();
+		timer_.resched(t);
+	} 
+}
+
+/**
+ * @brief next 
+ * 下一个时间点
+ * @param {double} interval
+ * @return {double}
+ */
+double BulkBackHandle::next(double interval)
+{
+	_execTime->next(interval);
+	return this->getNowTime();
+}
+
+/**
+ * @brief next 
+ * 下一个时间点
+ * @return {double}
+ */
+double BulkBackHandle::next()
+{
+	_execTime.next();
+	return this->getNowTime();
+}
+
+/**
+ * @brief getNowTime 
+ * 获得当前时间
+ * @return 
+ */
+double BulkBackHandle::getNowTime()
+{
+	return _execTime.getTime();
+}
---
+++ BulkBackHandle.h
@@ -0,0 +1,38 @@
+#ifndef _BULKBACKHANDLE_H_
+#define _BULKBACKHANDLE_H_
+#include "TimerHandler.h"
+#include "Timer.h"
+#include "BulkBackPressure.h"
+class BulkBackHandle;
+class BulkBackHandleTimer : public TimerHandler
+{
+	public:
+		BulkBackPressureTimer(BulkBackPressure* t): TimerHandler(), t_(t){}
+		BulkBackPressureTimer(): TimerHandler(){}
+		inline virtual void expire(Event*) {
+			t_->handle();
+			t_->timeout();
+			cout<<"expire"<<endl;
+		}
+	protected:
+		BulkBackHandle* t_;
+};
+
+class BulkBackHandle
+{
+	private:
+		BulkTime _execTime;
+	public:
+		BulkBackHandle():running_(0){}
+		virtual void handle(){}
+		void start();
+		void stop();
+		void timeout();
+		double next(double interval);
+		double next();
+		double getNowTime();
+	protected:
+		int running_;
+		BulkBackHandleTimer timer_;
+};
+#endif
